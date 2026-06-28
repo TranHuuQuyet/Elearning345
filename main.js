a0317df7ab8fdd783412f59a922a7463f02e494f
Tran Huu Quyet <huuquyet120406@gmail.com>
2026-06-28 22:12:51 +0700

    xong

--- main.js
+++ main.js
@@ -273,3 +273,10 @@ btnTop.addEventListener("click", () => {
     behavior: "smooth",
   });
 });
+//hbg
+const hamburger = document.getElementById("hamburger");
+const menu = document.querySelector(".menu-item");
+
+hamburger.addEventListener("click", () => {
+  menu.classList.toggle("show");
+});
